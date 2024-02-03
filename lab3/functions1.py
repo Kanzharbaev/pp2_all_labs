386bd41f49af598ee644401a62e50b2f497b418c
Kanzharbaev <157138771+Kanzharbaev@users.noreply.github.com>
2024-02-03 21:23:35 +0600

    Add files via upload

---
+++ lab3/functions1.py
@@ -0,0 +1,128 @@
+##A recipe you are reading states how many grams you need for the ingredient. Unfortunately, your store only sells items in ounces. Create a function to convert grams to ounces. ounces = 28.3495231 * grams
+def converter_ounces(gram):
+    ounces=28.3495231*gram
+    return ounces
+
+print(converter_ounces(300))
+##Read in a Fahrenheit temperature. Calculate and display the equivalent centigrade temperature. The following formula is used for the conversion: C = (5 / 9) * (F – 32)
+def converter_celsii(F):
+    C=(5/9)*(F-32)
+    return C
+
+print(converter_celsii(212))
+##Write a program to solve a classic puzzle: We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have? create function: solve(numheads, numlegs):
+def counter(head,legs):
+    chickens = (legs - (4*head)) / -2
+    rabits = head-chickens
+    return f"Rabits: {int(rabits)}\nChikens: {int(chickens)}"
+
+print(counter(35,94))
+##You are given list of numbers separated by spaces. Write a function filter_prime which will take list of numbers as an agrument and returns only prime numbers from the list.
+def filter_prime(lister):
+    prostoe = []
+    for i in lister:
+        count=0
+        if i==2:
+            prostoe.append(i)
+            continue
+        elif i==1:
+            continue
+        for j in range(2,i):
+            if(i%j==0):
+                count+=1
+        if(count==0):
+            prostoe.append(i)
+    return prostoe
+
+lister = [1,2,3,4,5,6,7,8,9,10]
+print(filter_prime(lister))    
+##Write a function that accepts string from user, return a sentence with the words reversed.
+def sentence(my_string):
+    word = my_string.split()
+    invert=' '.join(reversed(word))
+    return invert
+
+print(sentence("We are ready"))
+##Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+def tri(numbers):
+    for i in range(len(numbers)-1):
+        if(numbers[i]==numbers[i+1]==3):
+            return True
+    return False
+
+print(tri([1,2,3,3,4]))
+##Write a function that takes in a list of integers and returns True if it contains 007 in order
+def spy_game(nums):
+    my_list = []
+    for i in nums:
+        if i == 0:
+            my_list.append(i)
+        elif i == 7:
+            my_list.append(i)
+    
+    if len(my_list) >= 3 and my_list[:3] == [0, 0, 7]:
+        return True
+    return False
+
+print(spy_game([1,0,2,4,0,5,7]))
+
+##Write a function that computes the volume of a sphere given its radius.
+def volume(radius):
+    V=4/3*3.14*radius**3
+    return V
+    
+print(volume(5))
+
+##Write a Python function that takes a list and returns a new list with unique elements of the first list. Note: don't use collection set.
+def unique(sandar):
+    unique_sandar=[]
+    for i in sandar:
+        if i not in unique_sandar:
+            unique_sandar.append(i)
+    return unique_sandar
+
+sandar = [1,1,2,2,3,4,4,5]
+print(unique(sandar))
+
+##Write a Python function that checks whether a word or phrase is palindrome or not. Note: A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam
+def palindrom_sozder(string):
+    k=string[::-1]
+    if k == string:
+        return True
+    return False
+
+print(palindrom_sozder("madam"))
+
+##Define a functino histogram() that takes a list of integers and prints a histogram to the screen. For example, histogram([4, 9, 7]) should print the following:
+def histogram(chisla):
+    for i in chisla:
+        for j in range(i):
+            print("*", end="")
+        print()
+
+histogram([4,9,7])
+
+##Write a program able to play the "Guess the number" - game, where the number to be guessed is randomly chosen between 1 and 20. This is how it should work when run in a terminal:
+import random
+def guessGame():
+    guessnumber=random.randint(1,20)
+    tries=0
+    print("Hello! What is your name?")
+    name=input()
+    print(f"Well, {name}, I am thinking of a number between 1 and 20")
+    while True:
+        print("Take a guess.")
+        num=int(input())
+        tries+=1
+        if(num<guessnumber):
+            print("Your guess is too low.")
+        elif(num>guessnumber):
+            print("You guess is too big")
+        else:
+            print(f"Good job, {name}! You guessed my number in {tries} guesses!")
+            break
+guessGame()
+
+
+
+
